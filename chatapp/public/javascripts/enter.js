94a2b4dd6253c69c54c8fa538bbb2b389c037585
taikichu <20nc016@ms.dendai.ac.jp>
2022-08-23 19:03:46 +0900

    ログイン後のユーザー名実装

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -2,11 +2,11 @@
 
 // 入室メッセージをサーバに送信する
 // 入力されたユーザ名を取得する
-const userName = '';
+const userName = 'aaaaaaaaaa';
 // 入室メッセージイベントを送信する
 
 
 // サーバから受信した入室メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('sendMessageEvent', function (data) {
+    $('#thread').prepend('<p>' +  data +  '</p>');
 });
