35e78e6083fd0f3eab1fc007c09f99a42a004e0a
yori7 <yori.sypla7@gmail.com>
2022-08-23 18:49:41 +0900

    投稿処理（クライアント側）実装しました

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -1,17 +1,20 @@
 'use strict';
 
+const socketIo = require("socket.io")(server, { wsEngine: 'ws' });
+const io = socketIo.listen(server);
+
 // 投稿メッセージをサーバに送信する
 function publish() {
     // ユーザ名を取得
-    const userName = '';
+    const userName = $('#userName').text();
     // 入力されたメッセージを取得
-    const message = '';
+    const message = $('#message').val();
     // 投稿内容を送信
-
+    socket.emit('sendMessageEvent', userName + ': ' + message)
     return false;
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
-socket.on('', function (data) {
+socket.on('receiveMessageEvent', function (data) {
     $('#thread').prepend('<p>' + '</p>');
 });
