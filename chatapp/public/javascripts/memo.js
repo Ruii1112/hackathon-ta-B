c09d834f1b7e12d2d8c5025684de2a2067ccc102
maru5318 <ne201082@senshu-u.jp>
2022-08-23 18:58:58 +0900

    メモ実装

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -1,13 +1,23 @@
 'use strict';
 
+
+function getMemo(){
+    const memo = $('#message').val();
+    return memo
+}
+
+function getName(){
+    const name = $('#userName').val();
+    return name
+}
 // メモを画面上に表示する
 function memo() {
     // ユーザ名を取得
-    const userName = '';
+    const userName = getName();
     // 入力されたメッセージを取得
-    const message = '';
+    const message = getMemo();
     // メモの内容を表示
-    $('#thread').prepend('<p>' + '</p>');
+    $('#thread').prepend('<p>' + userName + 'さん' + '：' + message +'</p>');
 
     return false;
 }
