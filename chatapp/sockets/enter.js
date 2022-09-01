d744a8e31e8450f8987d9e8af4d4979fc2efa23d
yori7 <yori.sypla7@gmail.com>
2022-09-01 16:00:11 +0900

    db testing

--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -1,8 +1,16 @@
 'use strict';
 
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database("./db/database.db");
+db.run('create table if not exists users(id integer, name text)')
+db.run('delete from users');
+
+let id = 0;
+
 module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
     socket.on('sendEnterEvent', function (data) {
+        db.run('insert into users values(' + id++ + ', "' + data + '")')
         socket.broadcast.emit('receiveEnterEvent', data);
     });
 };
--- chatapp/sockets/exit.js
+++ chatapp/sockets/exit.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database("./db/database.db");
+
 module.exports = function (socket) {
     // 退室メッセージをクライアントに送信する
+    socket.on('sendExitEvent', function (data) {
+        db.run('delete from users where name="' + data + '"');
+        socket.broadcast.emit('receiveExitEvent', data)
+    })
 };
